3aa701a98fd37e15b8d1f763b65d51949db7cd3f
hanzalajahangir7 <118133960+hanzalajahangir7@users.noreply.github.com>
2024-06-29 22:26:43 +0500

    v1.2

---
+++ main.ts
@@ -0,0 +1,64 @@
+// for loop and while loop
+
+// for (let i = 0; i <5; i++){
+//     console.log(i)
+// }
+
+// let sum = 0
+// for(let a=0; a<5; a++){
+//     // sum = sum + a
+//     sum += a
+//     console.log(sum)
+// }
+
+// let a:string = "hanzala"
+
+// for(let i=0;i<=5;i++){
+//     console.log(`hello ${a}`)
+// }
+
+
+//for in loop 
+
+
+// let obj={
+//     student1 : "hanzala",
+//     student2 : "yoyo"
+// }
+
+// for (let x in obj){
+//     console.log(obj[x as keyof object]);
+// }
+
+// let obj1={
+//     student1 : "hanzala",
+//     student2 : "yoyo"
+// }
+
+// for(let key in obj1){
+//     console.log(obj1[key as keyof object])
+// }
+ 
+
+// for of loop
+
+// let arr:number[]=[1,2,3,4,5,6]
+
+// for (let value of arr){
+//     console.log(arr)
+// }
+
+// let bcc:string[]=["hanzala","asad","saad"]
+
+// for (let name of bcc){
+//     console.log(bcc)
+// }
+
+// while loop
+
+// let i = 0
+
+// while(i<=5){
+//     console.log(i)
+//     i++
+// }
